5fb79d0c0b24dd8dd276d02bbe84ae421e853060
Shin-Taro <sh.takahashi.113@gmail.com>
2023-11-06 16:20:04 +0900

    feat: add tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,20 @@ module.exports = {
   mode: "jit",
   content: ["./src/**/*.tsx"],
   theme: {
-    extend: {},
+    screens: {
+      sp: { max: "767px" },
+      pc: { min: "768px" },
+    },
+    extend: {
+      fontSize: {
+        first: "3.2rem",
+        second: "2.4rem",
+        third: "2rem",
+        fourth: "1.6rem",
+        fifth: "1.4rem",
+        sixth: "1.2rem",
+      },
+    },
   },
   plugins: [],
 }
