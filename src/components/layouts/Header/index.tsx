139de0841155ebb49e7b3c31a74d83178c615ab6
Shin-Taro <sh.takahashi.113@gmail.com>
2024-01-23 14:56:51 +0900

    refactor: fix export range

--- src/components/layouts/Header/index.tsx
+++ src/components/layouts/Header/index.tsx
@@ -1,5 +1,8 @@
 import { FC } from "react"
 
+/**
+ * @public
+ */
 export const Header: FC = () => (
   <header className="bg-sky-400 py-5 text-center text-first">
     <h1>Header</h1>
--- src/types/FcWithChildren/index.ts
+++ src/types/FcWithChildren/index.ts
@@ -2,4 +2,7 @@ import { FC, PropsWithChildren } from "react"
 
 type UnknownObject = Record<number | string, unknown>
 
+/**
+ * @public
+ */
 export type FcWithChildren<P = UnknownObject> = FC<PropsWithChildren<P>>
