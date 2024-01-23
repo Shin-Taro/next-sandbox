518a897fe423cba2d6fde5b8139da91124f6a41b
Shin-Taro <sh.takahashi.113@gmail.com>
2024-01-23 15:17:27 +0900

    chore: set up panda css

---
+++ src/pages/_app.tsx
@@ -0,0 +1,17 @@
+import { Header } from "@/components/layouts/Header"
+import "@/styles/globals.css"
+import type { AppProps } from "next/app"
+import { Inter } from "next/font/google"
+
+const inter = Inter({ subsets: ["latin"] })
+
+const App = ({ Component, pageProps }: AppProps) => {
+  return (
+    <main className={inter.className}>
+      <Header />
+      <Component {...pageProps} />
+    </main>
+  )
+}
+
+export default App
