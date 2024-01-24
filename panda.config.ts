c5a47b829306131a2e0f1f8bdc63417df4bb3f09
Shin-Taro <sh.takahashi.113@gmail.com>
2024-01-24 14:09:21 +0900

    chore: update panda.config

--- panda.config.ts
+++ panda.config.ts
@@ -1,19 +1,27 @@
+import { keyframes } from "@/shared/styles/keyframes"
 import { defineConfig } from "@pandacss/dev"
 
 export default defineConfig({
-  // Whether to use css reset
   preflight: true,
 
-  // Where to look for your css declarations
   include: ["./src/**/*.{js,jsx,ts,tsx}"],
 
-  // Files to exclude
   exclude: [],
 
-  // Useful for theme customization
-  theme: {
-    extend: {},
-  },
-
   emitPackage: true,
+
+  theme: {
+    extend: {
+      keyframes: {
+        ...keyframes,
+      },
+    },
+
+    tokens: {
+      colors: {
+        white: { value: "#FFFFFF" },
+        overlay: { value: "rgba(0, 0, 0, 0.5)" },
+      },
+    },
+  },
 })
