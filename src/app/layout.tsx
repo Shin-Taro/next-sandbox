3a57f20c29ff214e49f003c3a48e9149fe88e723
Shin-Taro <sh.takahashi.113@gmail.com>
2024-03-10 22:10:46 +0900

    feat: set up styled-components registry

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Header } from "@/components/layouts/Header"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import { ReactNode } from "react"
+import { StyledComponentsRegistry } from "@/lib/styled-components/StyledComponentsRegistry"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -15,8 +16,10 @@ const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Header />
-        <main className="mx-auto mt-[60px] w-[90%] text-third">{children}</main>
+        <StyledComponentsRegistry>
+          <Header />
+          <main className="mx-auto mt-[60px] w-[90%] text-third">{children}</main>
+        </StyledComponentsRegistry>
       </body>
     </html>
   )
