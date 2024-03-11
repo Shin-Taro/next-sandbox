21c32cb4c0ffd9c47cd335e6609bf0871d335d70
Shin-Taro <sh.takahashi.113@gmail.com>
2024-03-11 23:31:19 +0900

    feat: move directories

---
+++ src/app/_components/Header/index.tsx
@@ -0,0 +1,10 @@
+import { FC } from "react"
+
+/**
+ * @public
+ */
+export const Header: FC = () => (
+  <header className="border-2 border-sub py-5 text-center text-first shadow-dropMain">
+    <h1>Form on Next.js App Router</h1>
+  </header>
+)
